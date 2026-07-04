777c3d04481e2eb807accebe1b666ec58e071e37
SuparvaCode <suparva@outlook.in>
2026-07-04 21:51:30 +0530

    fix: Increase express JSON request payload size limit to 50mb

--- server/src/index.js
+++ server/src/index.js
@@ -37,7 +37,7 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
 app.use(morgan('dev'));
 
 // Serve built admin panel
